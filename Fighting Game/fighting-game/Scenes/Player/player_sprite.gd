37b726b12b77219a6102107771189cdcaa07dabd
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-09-22 13:57:04 -0300

    feat: update

--- Fighting Game/fighting-game/Scenes/Player/player_sprite.gd
+++ Fighting Game/fighting-game/Scenes/Player/player_sprite.gd
@@ -1,6 +1,9 @@
 extends AnimatedSprite3D
 
-func _process(delta):
+var facing_right := true
+
+func _process(_delta):
 	if material_override and sprite_frames:
 		var frame_tex = sprite_frames.get_frame_texture(animation, frame)
 		material_override.set_shader_parameter("sprite_texture", frame_tex)
+		material_override.set_shader_parameter("facing_right", facing_right)
--- Fighting Game/fighting-game/Scripts/Player/player.gd
+++ Fighting Game/fighting-game/Scripts/Player/player.gd
@@ -9,10 +9,11 @@ func _ready():
 	if sprite == null:
 		sprite = $PlayerSprite
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	handle_input()
 	move_character()
 	update_animation()
+	update_facing()
 
 func handle_input():
 	var input_vector := Vector2.ZERO
@@ -28,33 +29,20 @@ func handle_input():
 	velocity.z = direction.z * speed
 	velocity.y = 0
 
-	if velocity.x > 0:
-		facing_right = true
-	elif velocity.x < 0:
-		facing_right = false
-
 func move_character():
 	move_and_slide()
 
 func update_animation():
-	# Sprite principal
 	if velocity.length() < 0.1:
 		sprite.play("idle")
 	else:
 		sprite.play("walk")
-	
-	# Espelhamento do sprite principal
-	var s = sprite.scale
-	s.x = abs(s.x) if facing_right else -abs(s.x)
-	sprite.scale = s
 
-	# Atualiza PlayerShadow
-	if sprite.has_node("PlayerShadow"):
-		var shadow = sprite.get_node("PlayerShadow") as AnimatedSprite3D
-		shadow.animation = sprite.animation
-		shadow.frame = sprite.frame
-		
-		# Espelhamento X mantendo a rotação fixa
-		var shadow_scale = shadow.scale
-		shadow_scale.x = abs(shadow_scale.x) if facing_right else -abs(shadow_scale.x)
-		shadow.scale = shadow_scale
+func update_facing():
+	# Apenas espelhar scale.x, pois Billboard sempre olha para a câmera
+	if velocity.x > 0 and not facing_right:
+		facing_right = true
+		sprite.scale.x = abs(sprite.scale.x)
+	elif velocity.x < 0 and facing_right:
+		facing_right = false
+		sprite.scale.x = -abs(sprite.scale.x)
